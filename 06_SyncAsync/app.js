ae9a37d23532de91f0377e231235e4c6623a65db
sarfaraz.ahmed.a9@gmail.com <sarfaraz.ahmed.a9@gmail.com>
2023-12-28 01:30:01 +0530

    a

--- 06_SyncAsync/app.js
+++ 06_SyncAsync/app.js
@@ -1,22 +1,22 @@
-const { log } = require("console");
 const fs = require("fs");
 const os = require("os");
 
 console.log("1");
 
 //non- blocking
-fs.readFile("contact.txt","utf-8", (req, res) => {
+fs.readFile("./06_SyncAsync/contact.txt","utf-8", (req, res) => {
     console.log("the async result are ....");
     console.log(res);
 });
-
+/*
 console.log("2");
 //finding CPU count
 console.log(`the total CPU's are : ${os.cpus().length}`);
 
 console.log("11");
 //blocking
-const result = fs.readFileSync("contact.txt","utf-8");
+const result = fs.readFileSync("./06_SyncAsync/contact.txt","utf-8");
 console.log(result);
 
 console.log("12");
+*/
